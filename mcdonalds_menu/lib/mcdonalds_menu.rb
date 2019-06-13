dc101bfa906d96184d6c0e9f1977eaa6638ddb4b
Sara Harvey <saraharveynyc@gmail.com>
2019-06-13 18:40:19 +0000

    modified README

---
+++ mcdonalds_menu/lib/mcdonalds_menu.rb
@@ -0,0 +1,6 @@
+require "mcdonalds_menu/version"
+
+module McdonaldsMenu
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ mcdonalds_menu/lib/mcdonalds_menu/version.rb
@@ -0,0 +1,3 @@
+module McdonaldsMenu
+  VERSION = "0.1.0"
+end
